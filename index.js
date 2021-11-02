ebcefe831113cd57016404a1efbed7b402be99fe
Richardw2019 <rgarci3@clemson.edu>
2021-11-02 16:17:15 -0400

    Date and Time commit
    filters now output date to javascript and is the same format as the columns Date_time and Time

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var date, hour
+function filter_data() {
+  //this function gets the date from the filter box, the date is in the format 2012-12-01
+  //Year, Month, Day
+  date = document.getElementById("date").value;
+  hour = document.getElementById("hour").value;
+
+  //will probably need to clear data?
+  console.log(date)
+  console.log(hour)
+}
+
+
+
 const {DeckGL, HexagonLayer} = deck;
 
 const deckgl = new DeckGL({
@@ -26,9 +40,9 @@ let colorScale = d3.scaleLinear()
 //   [254, 173, 84],
 //   [209, 55, 78]
 // ];
-console.log(colorScale)
+//console.log(colorScale)
 renderLayer()
-console.log(colorScale(345))
+//console.log(colorScale(345))
 
 function renderLayer () {
   const hexLayer = new HexagonLayer({
@@ -40,10 +54,10 @@ function renderLayer () {
     getElevationValue: points => +points[0].hourly_counts,
     extruded: true,
     getPosition: d => {
-      console.log(d)
-      console.log(+d.hourly_counts)
-      console.log(d.mdate)
-      console.log(d.mdate === "0")
+      // console.log(d)
+      // console.log(+d.hourly_counts)
+      // console.log(d.mdate)
+      // console.log(d.mdate === "0")
       return [+d.longitude, +d.latitude]
     },
     opacity: 1,
--- scripts/join-data.py
+++ scripts/join-data.py
@@ -2,10 +2,17 @@
 
 import pandas as pd
 
-data1 = pd.read_csv('../data/nov_2019.csv')
-data2 = pd.read_csv('../data/sensor_locations.csv')
+#data1 = pd.read_csv('../data/nov_2019.csv')
+data1 = pd.read_csv('/Users/richardgarcia/Documents/GitHub/melbourne-pedestrian-visualization/data/nov_2019.csv')
+
+#data2 = pd.read_csv('../data/sensor_locations.csv')
+data2 = pd.read_csv('/Users/richardgarcia/Documents/GitHub/melbourne-pedestrian-visualization/data/sensor_locations.csv')
 
 output = pd.merge(data1, data2, on='sensor_id', how='left')
 
-print(output) 
-output.to_csv('../data/joined_data.csv')
+#cleans up the date_time column elminating time
+output['date_time'] = [x[:-13] for x in output['date_time']]
+
+
+#print(output) 
+output.to_csv('/Users/richardgarcia/Documents/GitHub/melbourne-pedestrian-visualization/data/joined_data.csv')
