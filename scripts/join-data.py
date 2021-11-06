ac53518948d47f067e9e8797383c301d095fee75
Richardw2019 <rgarci3@clemson.edu>
2021-11-06 18:34:03 -0400

    uploaded data

--- scripts/join-data.py
+++ scripts/join-data.py
@@ -3,16 +3,18 @@
 import pandas as pd
 
 #data1 = pd.read_csv('../data/nov_2019.csv')
-data1 = pd.read_csv('/Users/richardgarcia/Documents/GitHub/melbourne-pedestrian-visualization/data/nov_2019.csv')
+data1 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documents/GitHub/melbourne-pedestrian-visualization/data/full_2019_pedestrian.csv')
 
 #data2 = pd.read_csv('../data/sensor_locations.csv')
-data2 = pd.read_csv('/Users/richardgarcia/Documents/GitHub/melbourne-pedestrian-visualization/data/sensor_locations.csv')
+data2 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documents/GitHub/melbourne-pedestrian-visualization/data/sensor_locations.csv')
 
 output = pd.merge(data1, data2, on='sensor_id', how='left')
 
 #cleans up the date_time column elminating time
-output['date_time'] = [x[:-13] for x in output['date_time']]
+output['Date_Time'] = [x[:-13] for x in output['Date_Time']]
 
 
 #print(output) 
-output.to_csv('/Users/richardgarcia/Documents/GitHub/melbourne-pedestrian-visualization/data/joined_data.csv')
+output.to_csv("/Users/Richard Garcia/OneDrive - Clemson University/Desktop/4030/2019_pedestrian.csv")
+
+# "C:\Users\Richard Garcia\OneDrive - Clemson University\Desktop\4030"
