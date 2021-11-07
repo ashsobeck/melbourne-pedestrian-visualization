8e78045b1d7851473e5cb3d6cdfe683f7a8ebee8
Ashton Sobeck <asobeck@g.clemson.edu>
2021-11-07 13:51:17 -0500

    add in filter

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var date
 var hour
-
+const data = d3.csv('data/2019_pedestrian.csv')
+let dayData = data
 //this function gets the date from the filter box, the date is in the format 2012-12-01
 //Year, Month, Day
 function filter_data() {
 
   date = document.getElementById("date").value;
   hour = document.getElementById("hour").value;
-
+  dayData = data.then((dataset) => dataset.filter(d => {
+    // const dDate = d.date_time.split('/')
+    // const dateSplit = date.split('-')
+    (d.date_time.split('/')[0] == date.split('-')[1]) 
+    && (+d.date_time.split('/')[1] == +date.split('-')[2]) 
+  }))
+  // renderLayer()
   //will probably need to clear data?
   console.log(date)
   console.log(hour)
 }
-
 
 //when window loads, js gets the default filter values
 window.onload = function()
@@ -20,8 +26,25 @@ window.onload = function()
   filter_data();
 }
 
+
+console.log(dayData)
+// console.log(data)
 const {DeckGL, HexagonLayer} = deck;
 
+// renderLayer()
+// console.log(data)
+
+let colorScale = d3.scaleLinear()
+  .domain([0, 300])
+  .range(d3.schemeCategory10)
+// const COLOR_RANGE = [
+//   [1, 152, 189],
+//   [73, 227, 206],
+//   [216, 254, 181],
+//   [254, 237, 177],
+//   [254, 173, 84],
+//   [209, 55, 78]
+// ];
 const deckgl = new DeckGL({
   mapStyle: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
   initialViewState: {
@@ -59,21 +82,23 @@ renderLayer()
 
 function renderLayer () {
   const hexLayer = new HexagonLayer({
+    data: dayData,
     id: 'melbourne-pedestrian-density',
-    data,
     pickable: true,
     getColorValue: d => {
       return +d[0].hourly_counts.replace(/,/g,'')
     },
 
     getElevationValue: d => {
-      console.log(d[0].hourly_counts.replace(/,/g,''))
+      // console.log(d)
+      // console.log(d[0].hourly_counts.replace(/,/g,''))
       return +d[0].hourly_counts.replace(/,/g,'')
     },
     extruded: true,
     getPosition: (d, i) => {
       if (+d.time == hour ) {
-        return [+d.longitude, +d.latitude]
+      // console.log(d.time)
+      return [+d.longitude, +d.latitude]
       }
       else return []
     },
