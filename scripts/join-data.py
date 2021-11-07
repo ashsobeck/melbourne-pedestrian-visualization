99726882829354b774fb59097192ff85fe85f0f7
Richardw2019 <rgarci3@clemson.edu>
2021-11-06 20:03:36 -0400

    fixed dataset

--- scripts/join-data.py
+++ scripts/join-data.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 #data1 = pd.read_csv('../data/nov_2019.csv')
-data1 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documents/GitHub/melbourne-pedestrian-visualization/data/full_2019_pedestrian.csv')
+data1 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documents/GitHub/melbourne-pedestrian-visualization/data/new_csv_2019.csv')
 
 #data2 = pd.read_csv('../data/sensor_locations.csv')
 data2 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documents/GitHub/melbourne-pedestrian-visualization/data/sensor_locations.csv')
@@ -11,8 +11,7 @@ data2 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documen
 output = pd.merge(data1, data2, on='sensor_id', how='left')
 
 #cleans up the date_time column elminating time
-output['Date_Time'] = [x[:-13] for x in output['Date_Time']]
-
+output['date_time'] = output['date_time'].str[:9]
 
 #print(output) 
 output.to_csv("/Users/Richard Garcia/OneDrive - Clemson University/Desktop/4030/2019_pedestrian.csv")
