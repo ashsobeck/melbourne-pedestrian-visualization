2d22f0dcba6022b1efe717f65b92b14a2c85da34
Richardw2019 <rgarci3@clemson.edu>
2021-11-06 20:09:00 -0400

    fixed extreme weather dataset

--- scripts/join-data.py
+++ scripts/join-data.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 #data1 = pd.read_csv('../data/nov_2019.csv')
-data1 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documents/GitHub/melbourne-pedestrian-visualization/data/new_csv_2019.csv')
+data1 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documents/GitHub/melbourne-pedestrian-visualization/data/extreme_weather_Nov_21_2016.csv')
 
 #data2 = pd.read_csv('../data/sensor_locations.csv')
 data2 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documents/GitHub/melbourne-pedestrian-visualization/data/sensor_locations.csv')
@@ -11,9 +11,9 @@ data2 = pd.read_csv('/Users/Richard Garcia/OneDrive - Clemson University/Documen
 output = pd.merge(data1, data2, on='sensor_id', how='left')
 
 #cleans up the date_time column elminating time
-output['date_time'] = output['date_time'].str[:9]
+output['date_time'] = output['date_time'].str[:10]
 
 #print(output) 
-output.to_csv("/Users/Richard Garcia/OneDrive - Clemson University/Desktop/4030/2019_pedestrian.csv")
+output.to_csv("/Users/Richard Garcia/OneDrive - Clemson University/Desktop/4030/Nov_21_2016_extreme_weather.csv")
 
 # "C:\Users\Richard Garcia\OneDrive - Clemson University\Desktop\4030"
